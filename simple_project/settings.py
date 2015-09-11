18f60465b9e3c00d484abe03f30e9da5dcfe22ed
davisd <daniel.davis@excella.com>
2015-09-11 15:15:43 -0400

    Removed name from db

--- simple_project/settings.py
+++ simple_project/settings.py
@@ -78,7 +78,6 @@ WSGI_APPLICATION = 'simple_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
